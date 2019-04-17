48691c035f4eedab5bab549ebd0464995f918d86
笹沢裕一 <werimagin@gmail.com>
2019-04-17 11:53:32 +0900

    add

--- bleu_noninterro.py
+++ bleu_noninterro.py
@@ -54,7 +54,7 @@ p_noninterros=[]
 corenlp=CoreNLP()
 for p in tqdm(predicts):
     interro,p_noninterro=corenlp.forward(p)
-    p_noninterros.append(p_noninterro.split())
+    p_noninterros.append(p_noninterro)
 #target
 t_noninterros=[t.split() for t in noninterros]
 
