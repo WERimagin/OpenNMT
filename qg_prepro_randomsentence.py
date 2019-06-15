e83927a79065b9ad9e2b56e1c1237f4320dcdaac
笹沢裕一 <werimagin@gmail.com>
2019-06-15 18:05:42 +0900

    add

--- qg_prepro_randomsentence.py
+++ qg_prepro_randomsentence.py
@@ -23,7 +23,7 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument("--src", type=str, default="data/squad-src-val-interro.txt", help="input model epoch")
 parser.add_argument("--tgt", type=str, default="data/squad-tgt-val-interro.txt", help="input model epoch")
-parser.add_argument("--pred", type=str, default="pred.txt", help="input model epoch")
+parser.add_argument("--pred", type=str, default="data/squad-tgt-val-interro.txt", help="input model epoch")
 parser.add_argument("--interro", type=str, default="data/squad-interro-val-interro.txt", help="input model epoch")
 parser.add_argument("--noninterro", type=str, default="data/squad-noninterro-val-interro.txt", help="input model epoch")
 parser.add_argument("--p_noninterro", type=str, default="data/squad-pred_noninterro-val-interro.txt", help="input model epoch")
