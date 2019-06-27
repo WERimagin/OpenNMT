d973decd895c30f9a12b3cca9c447923c032ab63
笹沢裕一 <werimagin@gmail.com>
2019-06-27 13:00:25 +0900

    add

--- show_sentence.py
+++ show_sentence.py
@@ -61,7 +61,8 @@ for name in pred_name:
 
 np.random.seed(0)
 id_list=np.random.permutation(list(range(len(srcs))))
-for id in id_list[0:100]:
+for i,id in enumerate(id_list[0:100]):
+    print(i)
     print("SRC:{}".format(srcs[id]))
     print("TGT:{}".format(tgts[id]))
     for p in preds:
