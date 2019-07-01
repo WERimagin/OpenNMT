b380b6ecb4ca0faf3b3a75749c7d62e5d47e4f6d
笹沢裕一 <werimagin@gmail.com>
2019-07-01 17:52:08 +0900

    add

--- bert_prepro_modify.py
+++ bert_prepro_modify.py
@@ -155,8 +155,9 @@ def data_process(input_path,interro_path,modify_path,train,args):
                 #print(json.dumps(new_parapr,indent=4))
 
             new_topic["paragraphs"].append(new_paragraph)
-            print(json.dumps(new_paragraph,indent=4))
+            #print(json.dumps(new_paragraph,indent=4))
         new_data["data"].append(new_topic)
+        print(json.dumps(new_topic,indent=4))
 
     print(all_count)
     print(modify_count+1)
